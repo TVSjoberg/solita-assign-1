1ad2f953ca7257332881f0fc5fbb7e7f2e9f90d9
Torbjörn Sjöberg <t.sjoberg@sterna.se>
2020-10-16 09:28:38 +0200

    initial commit

---
+++ sqllite_testing.py
@@ -0,0 +1,32 @@
+import sqlite3
+from sqlite3 import Error
+from sqlite3.dbapi2 import sqlite_version
+import os
+import sys
+
+
+def create_connection(db_file):
+    """
+    create a connection to a db file and then close it
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite_version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+if __name__ == "__main__":
+
+    db_name = sys.argv[1] if sys.argv[1] else 'py_db'
+    path_to_db = 'sqlite3/'
+    try:
+        os.mkdir(path_to_db)
+    except OSError as e:
+        print(e)
+
+    create_connection(rf'{os.getcwd()}/sqlite3/{db_name}.db')
